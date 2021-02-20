727404c3669a9cceb9200b64668ebf98af844a9c
lijian <lijian6y@gmail.com>
2021-02-20 11:13:06 +0800

    update

--- my-test/src/main/java/com/llionframework/src/parse/v2/chapter02/BeanFactoryTest.java
+++ my-test/src/main/java/com/llionframework/src/parse/v2/chapter02/BeanFactoryTest.java
@@ -3,7 +3,7 @@ package com.llionframework.src.parse.v2.chapter02;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
@@ -11,15 +11,15 @@ import static org.junit.Assert.assertEquals;
 
 public class BeanFactoryTest {
 	public static void main(String[] args) {
-		testSimpleLoad();
+//		testSimpleLoad();
 	}
 
-	public static void testSimpleLoad(){
-		// XmlBeanFactory 已废弃，详见doc
-		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
-		MyTestBean bean=(MyTestBean) bf.getBean("myTestBean");
-		assertEquals("testStr",bean.getTestStr());
-	}
+//	public static void testSimpleLoad(){
+//		// XmlBeanFactory 已废弃，详见doc
+//		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+//		MyTestBean bean=(MyTestBean) bf.getBean("myTestBean");
+//		assertEquals("testStr",bean.getTestStr());
+//	}
 
 	/**
 	 * my
